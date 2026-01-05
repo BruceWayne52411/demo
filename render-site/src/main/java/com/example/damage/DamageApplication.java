a6e350aa835dcaf26b79e1107854f6be49d79bbb
Bruce Henson <158220789+BruceWayne52411@users.noreply.github.com>
2026-01-05 10:57:06 -0600

    ye

--- render-site/src/main/java/com/example/damage/DamageApplication.java
+++ render-site/src/main/java/com/example/damage/DamageApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.damage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DamageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DamageApplication.class, args);
 	}
 
 }
--- render-site/src/main/java/com/example/damage/DamageFormula.java
+++ render-site/src/main/java/com/example/damage/DamageFormula.java
@@ -1,4 +1,4 @@
-package com.example.demo.damage;
+package com.example.damage;
 
 public class DamageFormula {
 
